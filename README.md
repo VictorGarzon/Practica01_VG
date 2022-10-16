d44b8e63c4227aa567603226e60e1bd93cb1f670
Victor Garzón <vigaru11@hotmail.com>
2022-10-16 23:13:58 +0200

    domigo:zoom

---
+++ README.md
@@ -0,0 +1,5 @@
+# PRACTICA_01
+## HTML
+El html fue lo más sencillo en el body se encuentra una función al cargar el body (explicada abajo), para el titulo he utilizado header con h1, la parte de la izquierda un aside con cada img y separada con br , cada una tiene una clase única y uno tiene un id para saber cuál es el activo y también tiene una función de clic para cada única (explicada más abajo). Después se encuentra el section con el article con una función de clic (explicada más abajo) que contiene un img con donde se encontrar las imágenes en grande y por ultimo el div que contiene los h1 y h2 cada uno con un id para facilitar la inserción de datos
+
+## CSS
--- js/java.js
+++ js/java.js
@@ -1,59 +1,73 @@
 function loaded() {
     activo = document.getElementById("activo").className;
-    if (activo == "mini breda"){
+    if (activo == "breda"){
         activarb()
     }
-    if (activo == "mini venus"){
+    if (activo == "venus"){
         activarve()
     }
-    if (activo == "mini vulcano"){
+    if (activo == "vulcano"){
         activarvu()
     }
-    if (activo == "mini meninas"){
+    if (activo == "meninas"){
         activarm()
     }
-    if (activo == "mini inocencio"){
+    if (activo == "inocencio"){
         activari()
     }
 }
 
 function activarb() {
-    document.getElementsByClassName("mini")[1].removeAttribute("id");
-    document.getElementsByClassName("mini")[2].removeAttribute("id");
-    document.getElementsByClassName("mini")[3].removeAttribute("id");
-    document.getElementsByClassName("mini")[4].removeAttribute("id");
+    document.getElementById("activo").removeAttribute("id");
     document.getElementsByClassName("breda")[0].setAttribute("id","activo");
     document.getElementById("imagen").src="img/breda.jpg";
+    document.getElementById("nombre").innerText="RENDICIÓN DE BREDA";
+    document.getElementById("fecha").innerText="1634-35";
+    document.getElementById("precio").innerText="380€";
+    document.getElementById("descripcion").innerText="Réplica del cuadro a colores originales y tamaño 150 x 80cm";
 }
 function activarve() {
-    document.getElementsByClassName("mini")[0].removeAttribute("id");
-    document.getElementsByClassName("mini")[2].removeAttribute("id");
-    document.getElementsByClassName("mini")[3].removeAttribute("id");
-    document.getElementsByClassName("mini")[4].removeAttribute("id");
+    document.getElementById("activo").removeAttribute("id");
     document.getElementsByClassName("venus")[0].setAttribute("id","activo");
     document.getElementById("imagen").src="img/venus.jpg";
-}
-function activarvu() {
-    document.getElementsByClassName("mini")[0].removeAttribute("id");
-    document.getElementsByClassName("mini")[1].removeAttribute("id");
-    document.getElementsByClassName("mini")[3].removeAttribute("id");
-    document.getElementsByClassName("mini")[4].removeAttribute("id");
-    document.getElementsByClassName("vulcano")[0].setAttribute("id","activo");
-    document.getElementById("imagen").src="img/vulcano.jpg";
+    document.getElementById("nombre").innerText="VENUS DEL ESPEJO"
+    document.getElementById("fecha").innerText="1644-1648";
+    document.getElementById("precio").innerText="432€";
+    document.getElementById("descripcion").innerText="Réplica del cuadro a colores originales y tamaño 230 x 150cm";
+
 }
 function activarm() {
-    document.getElementsByClassName("mini")[0].removeAttribute("id");
-    document.getElementsByClassName("mini")[1].removeAttribute("id");
-    document.getElementsByClassName("mini")[2].removeAttribute("id");
-    document.getElementsByClassName("mini")[4].removeAttribute("id");
+    document.getElementById("activo").removeAttribute("id");
     document.getElementsByClassName("meninas")[0].setAttribute("id","activo");
     document.getElementById("imagen").src="img/meninas.jpg";
+    document.getElementById("nombre").innerText="LAS MENINAS"
+    document.getElementById("fecha").innerText="1616-1660";
+    document.getElementById("precio").innerText="1250€";
+    document.getElementById("descripcion").innerText="Réplica del cuadro a colores originales y tamaño 400 x 350cm";
+}
+function activarvu() {
+    document.getElementById("activo").removeAttribute("id");
+    document.getElementsByClassName("vulcano")[0].setAttribute("id","activo");
+    document.getElementById("imagen").src="img/vulcano.jpg";
+    document.getElementById("nombre").innerText="LA FRAGUA DE VULCANO"
+    document.getElementById("fecha").innerText="1630";
+    document.getElementById("precio").innerText="375€";
+    document.getElementById("descripcion").innerText="Réplica del cuadro a colores originales y tamaño 170 x 120cm";
 }
 function activari() {
-    document.getElementsByClassName("mini")[0].removeAttribute("id");
-    document.getElementsByClassName("mini")[1].removeAttribute("id");
-    document.getElementsByClassName("mini")[2].removeAttribute("id");
-    document.getElementsByClassName("mini")[3].removeAttribute("id");
+    document.getElementById("activo").removeAttribute("id");
     document.getElementsByClassName("inocencio")[0].setAttribute("id","activo");
     document.getElementById("imagen").src="img/inocencio.jpg";
+    document.getElementById("nombre").innerText="EL PAPA INOCENCIO X"
+    document.getElementById("fecha").innerText="1650";
+    document.getElementById("precio").innerText="180€";
+    document.getElementById("descripcion").innerText="Réplica del cuadro a colores originales y tamaño 80 x 140cm";
+}
+function zoom(){
+    if (document.getElementById("zoom") == null){
+        document.getElementsByTagName("article")[0].setAttribute("id", "zoom");
+    }
+    else{
+        document.getElementsByTagName("article")[0].removeAttribute("id");
+    }
 }
